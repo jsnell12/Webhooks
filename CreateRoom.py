b76f747275e7232440b67a32bcf93ce626367239
jsnell12 <s3531410@students.fscj.edu>
2022-11-20 10:05:29 -0500

    Create CreateRoom.py

---
+++ CreateRoom.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from getpass import getpass
+from pprint import pprint
+
+TOKEN = getpass('Paste your token: ')
+
+BASEURL = "https://webexapis.com"
+room = "/v1/rooms"
+
+body = {
+    'title': 'Javaris Room'
+}
+
+bodyJSON = json.dumps(body)
+
+headers = {
+    'Authorization': 'Bearer ' + TOKEN,
+    'Content-'
+}
+
+createRoom = BASEURL + room
+
+response = requests.post(createRoom, headers=headers, data=bodyJSON)
+
+responseJSON = resonse.json()
+
+pprint(responseJSON)
